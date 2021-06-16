2b2d41b23f25581727b6435d75edb9c4927a89ad
Purshi1988 <kumarpurushotham1988@gmail.com>
2021-06-16 16:30:37 +0530

    First Commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,27 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+		
+		
+		@Test
+		public void NativeAppAndriod()
+		{
+			System.out.println("NativeAppAndriod");
+			
+		}
+
+		@Test
+		public void IosApps()
+		{
+			
+			System.out.println("IosApps");
+		}
+
+
+
+
+
+}
+
